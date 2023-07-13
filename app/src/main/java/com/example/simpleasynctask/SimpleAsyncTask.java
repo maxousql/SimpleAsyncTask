16d72be9a860d2b78ab52db4800c009388827d9f
maxousql <maxlaiyio@outlook.fr>
2023-07-13 11:50:01 +0200

    add progress bar

--- app/src/main/java/com/example/simpleasynctask/SimpleAsyncTask.java
+++ app/src/main/java/com/example/simpleasynctask/SimpleAsyncTask.java
@@ -1,6 +1,7 @@
 package com.example.simpleasynctask;
 
 import android.os.AsyncTask;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.lang.ref.WeakReference;
@@ -8,8 +9,11 @@ import java.util.Random;
 
 public class SimpleAsyncTask extends AsyncTask <Void, Void, String> {
     private WeakReference<TextView> mTextView;
-    SimpleAsyncTask(TextView tv) {
+    private WeakReference<ProgressBar> mProgressBar;
+
+    SimpleAsyncTask(TextView tv, ProgressBar progressBar) {
         mTextView = new WeakReference<>(tv);
+        mProgressBar = new WeakReference<>(progressBar);
     }
 
     @Override
@@ -17,14 +21,32 @@ public class SimpleAsyncTask extends AsyncTask <Void, Void, String> {
         Random r = new Random();
         int n = r.nextInt(11);
         int s = n * 200;
+        int progress = 0;
+        int sleepTime = s / 100;
+
+        for (int i = 0; i < 100; i++) {
+            try {
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            progress++;
+            publishProgress(progress);
+        }
+
         try {
-            Thread.sleep(s);
+            Thread.sleep(s - progress * sleepTime);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
         return "Enfin réveillé après avoir dormi pendant " + s + " millisecondes !";
     }
 
+    private void publishProgress(int progress) {
+        mProgressBar.get().setProgress(progress);
+    }
     protected void onPostExecute(String result) {
         mTextView.get().setText(result);
     }
